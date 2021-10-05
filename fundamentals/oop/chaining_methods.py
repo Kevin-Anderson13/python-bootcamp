1aaa21ac6e91b70192f4751bf82e100b51032415
Kevin <kevinand1113@gmail.com>
2021-10-05 00:46:57 -0600

    chaining methods

--- fundamentals/oop/chaining_methods.py
+++ fundamentals/oop/chaining_methods.py
@@ -7,18 +7,22 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(f"{self.name} has {self.account_balance} left in their bank account.")
+        return self
 
     # def transfer_money(self, amount, other_user):
     #     self.account_balance -= amount
     #     other_user.account_balance += amount
     #     self.display_user_balance
     #     other_user.display_user_balance
+    #     return self
 
 kelsee = User("Kelsee Von Strucker", "kelsee@email.com")
 kevin = User("Kevin Anderson", "kevin@email.com")
@@ -28,30 +32,16 @@ print(kelsee.name)
 print(kevin.name)
 print(tyson.name)
 
-kelsee.make_deposit(100000)
-kelsee.make_deposit(1000000)
-kelsee.make_deposit(1277000)
-kelsee.make_withdrawl(1500000)
-
-kevin.make_deposit(10)
-kevin.make_deposit(50)
-kevin.make_withdrawl(20)
-kevin.make_withdrawl(30)
+kelsee.make_deposit(100000).make_deposit(1000000).make_deposit(1277000).make_withdrawl(1500000).display_user_balance()
 
 
-tyson.make_deposit(5)
-tyson.make_withdrawl(1)
-tyson.make_withdrawl(1)
-tyson.make_withdrawl(1)
+
+kevin.make_deposit(10).make_deposit(50).make_withdrawl(20).make_withdrawl(30).display_user_balance()
 
 
-print(kelsee.account_balance)
-print(kevin.account_balance)
-print(tyson.account_balance)
 
-kelsee.display_user_balance()
-kevin.display_user_balance()
-tyson.display_user_balance()
+tyson.make_deposit(5).make_withdrawl(1).make_withdrawl(1).make_withdrawl(1).display_user_balance()
+
+
 
 # kelsee.transfer_money(800000, kevin)
-

7bca717b72d034de476f075f44ac50381d5d21c6
Kevin <kevinand1113@gmail.com>
2021-10-18 04:47:35 -0600

    html tables from python back end

--- flask/fundamentals/tables/server.py
+++ flask/fundamentals/tables/server.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def names():
-    full_names= [
+def index():
+    names= [
         {'first_name' : 'Kevin', 'last_name' : 'Anderson'},
         {'first_name' : 'Kelsee', 'last_name' : 'Anderson'},
         {'first_name' : 'Tyson', 'last_name' : 'Galovich'},
         {'first_name' : 'Maddy', 'last_name' : 'Galovich'},
         {'first_name' : 'Zander', 'last_name' : 'Johnson'}
     ]
-    return render_template("index.html", names = full_names)
+    return render_template("index.html", names=names)
 
 
 if __name__=="__main__":

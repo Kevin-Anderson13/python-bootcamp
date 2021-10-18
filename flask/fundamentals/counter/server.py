694303288b8aea233f8d7e36c0b08718e9c0ead2
Kevin <kevinand1113@gmail.com>
2021-10-18 05:37:38 -0600

    session counter

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -3,7 +3,18 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'counterkey'
 
+@app.route('/')
+def index():
+    if "count" not in session:
+        session['count'] = 1
+    else:
+        session['count'] += 1
+    return render_template("index.html")
 
+@app.route('/clear')
+def reset_count():
+    session.clear()
+    return redirect('/')
 
 
 if __name__=="__main__":
